77288abf6a983368290221d1f1cddcd232a19811
Özgür Türkoğlu <138875953+ozgurtrko@users.noreply.github.com>
2025-01-28 13:23:21 +0300

    Update 1251. Average Selling Price.sql

--- SQL-Solutions/Easy/1251. Average Selling Price.sql
+++ SQL-Solutions/Easy/1251. Average Selling Price.sql
@@ -32,10 +32,8 @@ Write a solution to find the average selling price for each product. average_pri
 Return the result table in any order.
 """
 
-SELECT u.product_id,
-       ROUND(SUM(p.price * u.units)/SUM(u.units),2) AS average_price
-FROM unitssold u
-JOIN prices p ON 1=1
-    AND p.product_id = u.product_id 
-    AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY u.product_id 
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
